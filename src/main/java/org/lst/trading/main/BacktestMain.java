1cdbb34be8048431118802810ffcd9cd3f471a94
Roman <rpilyushin@gmail.com>
2023-11-20 16:50:56 +0200

    Refine BacktestMain.java

--- src/main/java/org/lst/trading/main/BacktestMain.java
+++ src/main/java/org/lst/trading/main/BacktestMain.java
@@ -13,82 +13,90 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
 public class BacktestMain {
-    static String alphaVantantageApiKey;
+    private static final Logger LOGGER = Logger.getLogger(BacktestMain.class.getName());
+    private static final String CONFIG_FILE = "config.properties";
+    private static final String API_KEY_PROPERTY = "alphavantantage.apikey";
+    private static String alphaVantageApiKey;
     private TradingStrategy strategy;
+    private int deposit;
+    private Backtest backtest;
 
-    public void setStrategy(TradingStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    // Result display method or class
-    public class ResultDisplay {
-        public static void displayResults(Backtest.Result result) {
-            // Display logic...
+    public static void main(String[] args) {
+        try {
+            loadProperties();
+            new BacktestMain().runBacktest();
+        } catch (Exception e) {
+            LOGGER.severe("Error running backtest: " + e.getMessage());
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        loadProperties();
-
+    private void runBacktest() throws IOException {
         String x = "AAPL";
         String y = "AMZN";
+        strategy = new CointegrationTradingStrategy(x, y);
+        HistoricalPriceService finance = new AlphaVantageHistoricalPriceService(alphaVantageApiKey);
+        MultipleDoubleSeries priceSeries = getPriceSeries(finance, x, y);
 
-        // initialize the trading strategy
-        TradingStrategy strategy = new CointegrationTradingStrategy(x, y);
-
-        // download historical prices
-        HistoricalPriceService finance = new AlphaVantageHistoricalPriceService(alphaVantantageApiKey);
-        MultipleDoubleSeries priceSeries = new MultipleDoubleSeries(finance.getHistoricalAdjustedPrices(x).toBlocking().first(), finance.getHistoricalAdjustedPrices(y).toBlocking().first());
-
-        // initialize the backtesting engine
-        int deposit = 15000;
-        Backtest backtest = new Backtest(deposit, priceSeries);
+        deposit = 15000;
+        backtest = new Backtest(deposit, priceSeries);
         backtest.setLeverage(4);
 
-        // do the backtest
         Backtest.Result result = backtest.run(strategy);
+        displayResults(result, priceSeries);
+    }
 
-        // show results
-        StringBuilder orders = new StringBuilder();
-        orders.append("id,amount,side,instrument,from,to,open,close,pl\n");
-        for (ClosedOrder order : result.getOrders()) {
-            orders.append(format(Locale.US, "%d,%d,%s,%s,%s,%s,%f,%f,%f\n", order.getId(), Math.abs(order.getAmount()), order.isLong() ? "Buy" : "Sell", order.getInstrument(), order.getOpenInstant(), order.getCloseInstant(), order.getOpenPrice(), order.getClosePrice(), order.getPl()));
-        }
-        System.out.print(orders);
+    private MultipleDoubleSeries getPriceSeries(HistoricalPriceService finance, String x, String y) throws IOException {
+        return new MultipleDoubleSeries(
+                finance.getHistoricalAdjustedPrices(x).toBlocking().first(),
+                finance.getHistoricalAdjustedPrices(y).toBlocking().first()
+        );
+    }
+
+    private void displayResults(Backtest.Result result, MultipleDoubleSeries priceSeries) {
+        String orders = formatOrders(result);
+        LOGGER.info(orders);
 
         int days = priceSeries.size();
+        LOGGER.info("\nBacktest result of " + strategy.getClass() + ": " + strategy);
+        LOGGER.info("Prices: " + priceSeries);
+        LOGGER.info(format(Locale.US, "Simulated %d days, Initial deposit %d, Leverage %f",
+                priceSeries.size(), deposit, backtest.getLeverage()));
+        LOGGER.info(format(Locale.US, "Commissions = %f", result.getCommissions()));
+        LOGGER.info(format(Locale.US, "P/L = %.2f, Final value = %.2f, Result = %.2f%%, Annualized = %.2f%%, Sharpe (rf=0%%) = %.2f",
+                result.getPl(), result.getFinalValue(), result.getReturn() * 100, result.getReturn() / (days / 251.) * 100, result.getSharpe()));
 
-        System.out.println();
-        System.out.println("Backtest result of " + strategy.getClass() + ": " + strategy);
-        System.out.println("Prices: " + priceSeries);
-        System.out.println(format(Locale.US, "Simulated %d days, Initial deposit %d, Leverage %f", days, deposit, backtest.getLeverage()));
-        System.out.println(format(Locale.US, "Commissions = %f", result.getCommissions()));
-        System.out.println(format(Locale.US, "P/L = %.2f, Final value = %.2f, Result = %.2f%%, Annualized = %.2f%%, Sharpe (rf=0%%) = %.2f", result.getPl(), result.getFinalValue(), result.getReturn() * 100, result.getReturn() / (days / 251.) * 100, result.getSharpe()));
+        LOGGER.info("Orders: " + Util.writeStringToTempFile(orders));
+        LOGGER.info("Statistics: " + Util.writeCsv(new MultipleDoubleSeries(result.getPlHistory(), result.getMarginHistory())));
+    }
 
-        System.out.println("Orders: " + Util.writeStringToTempFile(orders.toString()));
-        System.out.println("Statistics: " + Util.writeCsv(new MultipleDoubleSeries(result.getPlHistory(), result.getMarginHistory())));
+    private static String formatOrders(Backtest.Result result) {
+        return result.getOrders().stream()
+                .map(order -> format(Locale.US, "%d,%d,%s,%s,%s,%s,%f,%f,%f",
+                        order.getId(), Math.abs(order.getAmount()), order.isLong() ? "Buy" : "Sell",
+                        order.getInstrument(), order.getOpenInstant(), order.getCloseInstant(),
+                        order.getOpenPrice(), order.getClosePrice(), order.getPl()))
+                .collect(Collectors.joining("\n", "id,amount,side,instrument,from,to,open,close,pl\n", ""));
     }
 
     private static void loadProperties() {
-        try (InputStream input = BacktestMain.class.getClassLoader().getResourceAsStream("config.properties")) {
+        try (InputStream input = BacktestMain.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
             Properties prop = new Properties();
 
             if (input == null) {
-                System.out.println("Sorry, unable to find config.properties");
+                LOGGER.warning("Sorry, unable to find " + CONFIG_FILE);
                 return;
             }
 
-            // load a properties file from class path
             prop.load(input);
-
-            // get the property value
-            alphaVantantageApiKey = prop.getProperty("alphavantantage.apikey");
+            alphaVantageApiKey = prop.getProperty(API_KEY_PROPERTY);
         } catch (IOException ex) {
-            ex.printStackTrace();
+            LOGGER.severe("Error loading properties: " + ex.getMessage());
         }
     }
 }
